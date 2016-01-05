34c9e28f47909b734322df3e2d32b17c5be26e68
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-05 14:20:48 -0800

    put file into specific uploads folder

--- server/api/submit/submitRoutes.js
+++ server/api/submit/submitRoutes.js
@@ -2,17 +2,23 @@ var router = require('express').Router();
 var logger = require('../../util/logger');
 var fs = require('fs');
 var path = require('path');
+var url = require('url');
 router.route('/')
   .get(function(req, res, next) {
     console.log(req);
   })
   .put(function(req, res, next) {
-    var outStream = fs.createWriteStream(path.resolve(__dirname, "../../../uploads/testfile.pdf"));
+    console.log('parsed url');
+    var query = url.parse(req.url, true).query;
+    console.log(query);
+    console.log(query.class.toUpperCase());
+    var filename = query.class.toUpperCase() + "/" + query.assignment.toUpperCase() + ".pdf";
+    var outStream = fs.createWriteStream(path.resolve(__dirname, "../../../uploads/" + filename));
     req.pipe(outStream);
-    // console.log('req');
-    // console.log(req);
-    // console.log('req-body');
-    // console.log(req.body);
+    console.log('req');
+    console.log(req);
+    console.log('req-body');
+    console.log(req.body);
     res.send("file uploaded");
   });
 

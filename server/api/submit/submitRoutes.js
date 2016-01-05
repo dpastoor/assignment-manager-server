7379a8a902b78225702dd13fcde02199621dff21
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-05 13:56:25 -0800

    actually use proper put for accepting files

--- server/api/submit/submitRoutes.js
+++ server/api/submit/submitRoutes.js
@@ -1,16 +1,18 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
-
-
+var fs = require('fs');
+var path = require('path');
 router.route('/')
   .get(function(req, res, next) {
     console.log(req);
   })
-  .post(function(req, res, next) {
-    console.log('req');
-    console.log(req);
-    console.log('req-body');
-    console.log(req.body);
+  .put(function(req, res, next) {
+    var outStream = fs.createWriteStream(path.resolve(__dirname, "../../../uploads/testfile.pdf"));
+    req.pipe(outStream);
+    // console.log('req');
+    // console.log(req);
+    // console.log('req-body');
+    // console.log(req.body);
     res.send("file uploaded");
   });
 
